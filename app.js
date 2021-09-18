e396d72b9e7b6d0fa4321fedd0f4380629270346
NARESHKUMAR GOUD <90695464+naresh-goud@users.noreply.github.com>
2021-09-18 16:05:48 +0530

    upto translation

--- app.js
+++ app.js
@@ -2,9 +2,29 @@ var btnTranslate= document.querySelector("#btn-translate")
 var txtInput=document.querySelector("#txt-Input")
 var outputDiv=document.querySelector("#output")
 
+//var serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL="https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(input){
+    return serverURL +"?" +"text=" + input
+}
+
+function errorHandler(error){
+    console.log("error occured", error)
+    alert("something went wrong")
+}
+
 
 function clickHandler() {
-   
-    outputDiv.innerText="khsghlkjshg " + txtInput.value;
+   //outputDiv.innerText="khsghlkjshg " + txtInput.value;
+   var inputText = txtInput.value;
+
+   fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText =translatedText;
+    })
+    .catch(errorHandler)
 };
 btnTranslate.addEventListener ( "click", clickHandler)
